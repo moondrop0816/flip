8a3957b3b6ee90cb85c6dee35772cb60fa54fc67
moondrop0816 <moondrop0816@gmail.com>
2024-04-23 14:07:24 +0900

    [FIX] 배포테스트용 에러처리 #3

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -84,48 +84,23 @@ const SignUp = () => {
 
   const onSubmit: SubmitHandler<UserInfo> = async (data) => {
     event?.preventDefault()
-    try {
-      const credential = await createUserWithEmailAndPassword(
-        auth,
-        data.email,
-        data.password
-      )
-      const uid = credential.user.uid
-      const profileImgUrl = await imageUpload(uid)
-      const userDB = collection(db, 'user')
-      const userDoc = doc(userDB, uid)
-      await setDoc(userDoc, {
-        id: uid,
-        email: data.email,
-        nickname: data.nickname,
-        bio: data.bio,
-        createdAt: Date.now(),
-        profileImg: profileImgUrl,
-      })
-    } catch (error) {
-      // TODO : 에러처리
-      // 인증 삭제
-      // 프로필 이미지 삭세
-      // 유저 문서 삭제
-      // if (error instanceof Error) {
-      //   switch (
-      //     error.code
-      //   ) {
-      //     case 'auth/invalid-email':
-      //       alert('올바른 이메일 형식이 아닙니다.')
-      //       break
-      //     case 'auth/weak-password':
-      //       alert('비밀번호가 너무 쉬워요.')
-      //       break
-      //     case 'auth/email-already-in-use':
-      //       alert('등록된 이메일 입니다.')
-      //       break
-      //     default:
-      //       alert('회원가입에 실패했습니다. 잠시 후 다시 시도해 주세요.')
-      //       break
-      //   }
-      // }
-    }
+    const credential = await createUserWithEmailAndPassword(
+      auth,
+      data.email,
+      data.password
+    )
+    const uid = credential.user.uid
+    const profileImgUrl = await imageUpload(uid)
+    const userDB = collection(db, 'user')
+    const userDoc = doc(userDB, uid)
+    await setDoc(userDoc, {
+      id: uid,
+      email: data.email,
+      nickname: data.nickname,
+      bio: data.bio,
+      createdAt: Date.now(),
+      profileImg: profileImgUrl,
+    })
   }
 
   return (
