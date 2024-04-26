ca22d3e681ca31505d1d6f859f06acd419f0e3f3
moondrop0816 <moondrop0816@gmail.com>
2024-04-27 00:52:22 +0900

    [CHORE] 로그인 확인 로직 및 로그인한 유저 정보 로컬 스토리지에 저장 #26

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,15 +1,53 @@
+'use client'
+
 import Link from 'next/link'
 import { Button } from '../ui/button'
+import { onAuthStateChanged, signOut } from 'firebase/auth'
+import { auth, db } from '@/firebase/firebase'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { UserInfo } from '@/types/user'
 
 const Header = () => {
+  const router = useRouter()
+  const userCheck = () => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid
+        const q = query(collection(db, 'user'), where('uid', '==', uid))
+        const querySnapshot = await getDocs(q)
+        const data = querySnapshot.docs.map((doc) => doc.data())[0]
+        if (!localStorage.getItem('currentUser')) {
+          localStorage.setItem('currentUser', JSON.stringify(data))
+        }
+      } else {
+        // User is signed out
+        localStorage.removeItem('currentUser')
+        router.push('/login')
+      }
+    })
+  }
+
+  const currentUser = localStorage.getItem('currentUser') || ''
+  const currentData: UserInfo = JSON.parse(currentUser)
+
+  const onLogout = async () => {
+    await signOut(auth)
+    localStorage.removeItem('currentUser')
+  }
+
+  useEffect(() => {
+    userCheck()
+  }, [])
+
   return (
     <header className='bg-slate-300 shadow-md flex flex-wrap justify-between items-center px-5 py-3 sticky'>
       <div className='basis-1/3 text-left'>
         <div className='rounded-full overflow-hidden w-10'>
-          <img
-            src='https://firebasestorage.googleapis.com/v0/b/flip-fb254.appspot.com/o/3OCYbv1g8shEE1k7N8faT7jjZkm1%2F1610707726106.jpg?alt=media&token=515075ef-17fa-4400-b462-cf45de81f395'
-            alt='프로필 이미지'
-          />
+          {currentData && (
+            <img src={currentData.profileImg} alt='프로필 이미지' />
+          )}
         </div>
       </div>
       <div className='basis-1/3 text-center'>
@@ -18,9 +56,15 @@ const Header = () => {
         </Link>
       </div>
       <div className='basis-1/3 self-end text-right'>
-        <Button variant={'ghost'}>
-          <Link href={'/login'}>로그인</Link>
-        </Button>
+        {currentData ? (
+          <Button variant={'ghost'} onClick={onLogout}>
+            <Link href={'/login'}>로그아웃</Link>
+          </Button>
+        ) : (
+          <Button variant={'ghost'}>
+            <Link href={'/login'}>로그인</Link>
+          </Button>
+        )}
       </div>
     </header>
   )
