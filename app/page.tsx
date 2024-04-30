aeef0b75ddb0f8ef61e60b6567e47c24f1d16c19
moondrop0816 <moondrop0816@gmail.com>
2024-04-30 21:43:42 +0900

    [FEAT] 모든 게시글이 최신순으로 조회되는 feed 페이지 구현 #28

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,50 @@
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
 import withAuth from '@/components/hocs/withAuth'
+import PostCard from '@/components/post/postCard'
+import { collection, getDocs, orderBy, query } from 'firebase/firestore'
+import { db } from '@/firebase/firebase'
+import { useEffect, useState } from 'react'
+import { Post } from '@/types/post'
 
 function Home() {
+  const [feed, setFeed] = useState<{ id: string; data: Post }[]>([])
+
+  const getFeedData = async () => {
+    try {
+      const q = query(collection(db, 'feed'), orderBy('createdAt', 'desc'))
+      const querySnapshot = await getDocs(q)
+      const data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: {
+          userId: doc.data().userId,
+          content: doc.data().content,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          createdAt: doc.data().createdAt,
+          updatedAt: doc.data().updatedAt,
+          imageUrl: doc.data().imageUrl,
+        },
+      }))
+      setFeed(data)
+    } catch (error) {
+      console.error('Error fetching user info:', error)
+    }
+  }
+
+  useEffect(() => {
+    getFeedData()
+  }, [])
+
   return (
     <>
       <Header />
-      <div className='container'>
-        <div>로그인한 유저</div>
-        <div>닉네임님 환영합니다!</div>
-      </div>
+      {feed.map((feedData) => {
+        return (
+          <PostCard key={feedData.id} id={feedData.id} data={feedData.data} />
+        )
+      })}
+
       <Footer isAddible={true} />
     </>
   )
