95cc41ec610990e8b2928c16061c6b99b2eb684e
moondrop0816 <moondrop0816@gmail.com>
2024-05-02 11:38:00 +0900

    [FEAT] 무한스크롤 적용 및 hook으로 분리 #28

--- app/page.tsx
+++ app/page.tsx
@@ -4,49 +4,32 @@ import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
 import withAuth from '@/components/hocs/withAuth'
 import PostCard from '@/components/post/postCard'
-import { collection, getDocs, orderBy, query } from 'firebase/firestore'
-import { db } from '@/firebase/firebase'
-import { useEffect, useState } from 'react'
-import { Post } from '@/types/post'
+import { InfiniteScroll } from '@/types/post'
+import React from 'react'
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll'
 
 function Home() {
-  const [feed, setFeed] = useState<{ id: string; data: Post }[]>([])
-
-  const getFeedData = async () => {
-    try {
-      const q = query(collection(db, 'feed'), orderBy('createdAt', 'desc'))
-      const querySnapshot = await getDocs(q)
-      const data = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        data: {
-          userId: doc.data().userId,
-          content: doc.data().content,
-          commentCount: doc.data().commentCount,
-          likeCount: doc.data().likeCount,
-          createdAt: doc.data().createdAt.toDate(),
-          updatedAt: doc.data().updatedAt.toDate(),
-          imageUrl: doc.data().imageUrl,
-        },
-      }))
-      setFeed(data)
-    } catch (error) {
-      console.error('Error fetching user info:', error)
-    }
+  const feedObj: InfiniteScroll = {
+    dbName: 'feed',
+    limitNum: 5,
   }
-
-  useEffect(() => {
-    getFeedData()
-  }, [])
+  const { items, hasNextPage } = useInfiniteScroll(feedObj)
 
   return (
     <>
       <Header />
-      {feed.map((feedData) => {
-        return (
-          <PostCard key={feedData.id} id={feedData.id} data={feedData.data} />
-        )
-      })}
-
+      {items?.map((page, pageIndex) => (
+        <React.Fragment key={pageIndex}>
+          {page?.map((feedData) => (
+            <PostCard key={feedData.id} id={feedData.id} data={feedData.data} />
+          ))}
+        </React.Fragment>
+      ))}
+      {!hasNextPage && (
+        <div className='mb-16 p-6 pb-20 text-center text-slate-400'>
+          마지막 게시글입니다.
+        </div>
+      )}
       <Footer isAddible={true} />
     </>
   )
