2cf0353a29039e10af0b9d86f3f75aa7fc161017
moondrop0816 <moondrop0816@gmail.com>
2024-05-03 23:15:19 +0900

    [CHORE] 백그라운드 포지션 수정 #32

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { FadeLoader } from 'react-spinners'
 
 export default function Loading() {
   return (
-    <div className='w-screen h-screen bg-slate-500 fixed top-0 left-0 z-50'>
+    <div className='w-screen h-screen bg-slate-500 top-0 left-0 z-50'>
       <div className='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4'>
         <FadeLoader color='#fff' />
       </div>
