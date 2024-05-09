40594cdeb3a0183c8001668dab680bb9c40f97b5
moondrop0816 <moondrop0816@gmail.com>
2024-05-10 05:13:45 +0900

    [CHORE] loginUserContext 적용 #36

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,33 +3,14 @@
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
-import { auth, db, userDB } from '@/firebase/firebase'
-import { useEffect, useState } from 'react'
+import { auth, userDB } from '@/firebase/firebase'
+import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { getDocs, query, where } from 'firebase/firestore'
 import { useLoginUserInfo } from '@/context/loginUserInfoProvider'
 
 const Header = () => {
-  // const [loginUser, setLoginUser] = useState({
-  //   userId: '',
-  //   profileImg: './defaultProfile.png',
-  // })
   const router = useRouter()
-  // const userCheck = () => {
-  //   onAuthStateChanged(auth, async (user) => {
-  //     if (user) {
-  //       const uid = user.uid
-  //       const q = query(collection(db, 'user'), where('uid', '==', uid))
-  //       const querySnapshot = await getDocs(q)
-  //       const data = querySnapshot.docs.map((doc) => doc.data())[0]
-  //       setLoginUser({ userId: data.userId, profileImg: data.profileImg })
-  //     } else {
-  //       setLoginUser({ userId: '', profileImg: './defaultProfile.png' })
-  //       router.push('/login')
-  //     }
-  //   })
-  // }
-
   const { loginUserInfo, setLoginUserInfo } = useLoginUserInfo()
 
   const getLoginUser = () => {
@@ -62,7 +43,6 @@ const Header = () => {
   }
 
   useEffect(() => {
-    // userCheck()
     getLoginUser()
   }, [])
 
@@ -70,7 +50,7 @@ const Header = () => {
     <header className='bg-slate-300 shadow-md flex flex-wrap justify-between items-center px-5 py-3 sticky'>
       <div className='basis-1/3 text-left'>
         <div className='rounded-full overflow-hidden w-10'>
-          <Link href={`/mypage/${loginUserInfo?.userId}`} scroll={false}>
+          <Link href={`/user/${loginUserInfo?.userId}`} scroll={false}>
             <img src={loginUserInfo?.profileImg} alt='프로필 이미지' />
           </Link>
         </div>
