56e4ba74b68c0e08d9dca60c9f0b4fcd07204f37
moondrop0816 <moondrop0816@gmail.com>
2024-05-10 05:15:24 +0900

    [FEAT] 팔로우-언팔로우 기능 구현 #36

--- app/(login)/user/[id]/page.tsx
+++ app/(login)/user/[id]/page.tsx
@@ -1,81 +1,66 @@
 'use client'
 
-import { db } from '@/firebase/firebase'
-import { collection, getDocs, query, where } from 'firebase/firestore'
-import { MypageInfo } from '@/types/user'
+import { userDB } from '@/firebase/firebase'
+import { getDocs, query, where } from 'firebase/firestore'
 import { Button } from '@/components/ui/button'
 import Icon from '@/components/icon'
-import { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import withAuth from '@/components/hocs/withAuth'
 import Link from 'next/link'
 import { useLoginUserInfo } from '@/context/loginUserInfoProvider'
+import { BtnFollow } from '@/components/follow/btnFollow'
+import { useQuery } from '@tanstack/react-query'
 
 const MyPage = () => {
   const path = usePathname()
-  const [userInfo, setUserInfo] = useState<MypageInfo>({
-    userId: '',
-    nickname: '',
-    bio: '',
-    profileImg: './defaultProfile.png',
-    followerCount: 0,
-    followingCount: 0,
-  })
-
   const { loginUserInfo } = useLoginUserInfo()
 
-  const getUserInfo = async (userId: string) => {
-    try {
-      const q = query(collection(db, 'user'), where('userId', '==', userId))
+  const { data: userInfo } = useQuery({
+    queryKey: ['user'],
+    queryFn: async () => {
+      const userId = path.split('/')[path.split('/').length - 1]
+      const q = query(userDB, where('userId', '==', userId))
       const querySnapshot = await getDocs(q)
-      const data = querySnapshot.docs.map((doc) => doc.data())[0]
-      setUserInfo({
-        userId: data.userId,
-        nickname: data.nickname,
-        bio: data.bio,
-        profileImg: data.profileImg,
-        followerCount: data.followerCount,
-        followingCount: data.followingCount,
-      })
-    } catch (error) {
-      console.error('Error fetching user info:', error)
-    }
-  }
-
-  useEffect(() => {
-    getUserInfo(path.split('/')[path.split('/').length - 1])
-  }, [])
+      const data = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        data: doc.data(),
+      }))[0]
+      return data
+    },
+  })
 
   return (
     <section>
       <div>
         <div className='flex justify-between items-start'>
           <div className='rounded-full overflow-hidden w-1/6 mb-5'>
-            <img src={userInfo.profileImg} alt='프로필 이미지' />
+            <img src={userInfo?.data.profileImg} alt='프로필 이미지' />
           </div>
           <div className='flex items-center gap-1'>
-            {loginUserInfo?.userId === userInfo.userId ? (
-              <Button>프로필 수정</Button>
+            {loginUserInfo?.userId === userInfo?.data.userId ? (
+              <Button variant={'outline'}>프로필 수정</Button>
             ) : (
-              <Button>
-                <Icon name='Send' className='mr-2 h-4 w-4' />
-                DM
-              </Button>
-              // TODO: 팔로우 버튼 추가해주기
+              <>
+                <Button variant={'outline'}>
+                  <Icon name='Send' className='mr-2 h-4 w-4' />
+                  DM
+                </Button>
+                <BtnFollow followingUserUid={userInfo?.id} />
+              </>
             )}
           </div>
         </div>
-        <p className='text-lg font-bold'>{userInfo.nickname}</p>
-        <p className='text-gray-500 mb-2'>{`@${userInfo.userId}`}</p>
-        <p>{userInfo.bio}</p>
-        <div>
-          <div>
-            <span>팔로잉</span>
-            <Link href={'/feed'}>{userInfo.followingCount}</Link>
+        <p className='text-lg font-bold'>{userInfo?.data.nickname}</p>
+        <p className='text-gray-500 mb-2'>{`@${userInfo?.data.userId}`}</p>
+        <p>{userInfo?.data.bio}</p>
+        <div className='flex justify-start items-center gap-5 mt-2'>
+          <div className='flex items-center gap-1'>
+            <span className='font-semibold'>팔로잉</span>
+            <Link href={'/feed'}>{userInfo?.data.followingCount}</Link>
           </div>
-          <div>
-            <span>팔로워</span>
-            <Link href={'/feed'}>{userInfo.followerCount}</Link>
+          <div className='flex items-center gap-1'>
+            <span className='font-semibold'>팔로워</span>
+            <Link href={'/feed'}>{userInfo?.data.followerCount}</Link>
           </div>
         </div>
       </div>
---
+++ components/follow/btnFollow.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import { useLoginUserInfo } from '@/context/loginUserInfoProvider'
+import { Button } from '../ui/button'
+import {
+  deleteDoc,
+  doc,
+  getDocs,
+  increment,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from 'firebase/firestore'
+import { followDB, userDB } from '@/firebase/firebase'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+export const BtnFollow = ({
+  followingUserUid,
+}: {
+  followingUserUid: string | undefined
+}) => {
+  const { loginUserInfo } = useLoginUserInfo()
+  const queryClient = useQueryClient()
+  // 팔로우 로직 정리
+  // 버튼의 조건부 렌더링
+  // 팔로우 버튼은 로그인한 사용자가 아닌 사용자에게 나타나야 함
+  // 이 사용자가 로그인한 유저의 팔로잉 목록에 없다 => 팔로우 버튼
+  // 이 사용자가 로그인한 유저의 팔로잉 목록에 있다 => 언팔로우 버튼
+
+  const { data: isFollowing } = useQuery({
+    queryKey: ['isFollowing', loginUserInfo?.uid, followingUserUid],
+    queryFn: async () => {
+      const q = query(
+        followDB,
+        where('followerUserId', '==', loginUserInfo?.uid),
+        where('followingUserId', '==', followingUserUid)
+      )
+      const querySnapshot = await getDocs(q)
+      return querySnapshot
+    },
+  })
+
+  // 팔로우 로직
+  // followDB에 내 아이디, 상대 아이디로 문서 생성(각자 uid로 넣어주기)
+  // 내 유저 문서에 팔로잉 1 증가
+  // 상대 유저 문서에 팔로워 1 증가
+  const mutateFollow = useMutation({
+    mutationFn: async () => {
+      // 내 문서 찾기
+      const myDoc = doc(userDB, loginUserInfo?.uid)
+      // 상대 문서 찾기
+      const followingDoc = doc(userDB, followingUserUid)
+      // 팔로우 디비 문서 생성(자동 아이디)
+      const followRef = doc(followDB)
+      // 팔로우 디비 문서 수정(uid 추가)
+      // 팔로우 = 내가 상대의 팔로워가 됨
+      await setDoc(followRef, {
+        followerUserId: loginUserInfo?.uid,
+        followingUserId: followingUserUid,
+      })
+      // 내 문서 팔로잉 증가
+      await updateDoc(myDoc, {
+        followingCount: increment(1),
+      })
+      // 상대 유저 문서 팔로워 증가
+      await updateDoc(followingDoc, {
+        followerCount: increment(1),
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['user'],
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['isFollowing'],
+      })
+    },
+  })
+
+  // 언팔로우 로직
+  // followDB에 내 아이디, 상대 아이디로 있는 문서 제거(각각 uid로 넣어주기)
+  // 내 문서에 팔로잉 1 감소
+  // 상대 유저 문서에 1 감소
+  const mutateUnfollow = useMutation({
+    mutationFn: async () => {
+      // 내 문서 찾기
+      const myDoc = doc(userDB, loginUserInfo?.uid)
+      // 상대 문서 찾기
+      const followingDoc = doc(userDB, followingUserUid)
+      // 팔로우 디비 문서 삭제
+      const q = query(
+        followDB,
+        where('followerUserId', '==', loginUserInfo?.uid),
+        where('followingUserId', '==', followingUserUid)
+      )
+      const querySnapshot = await getDocs(q)
+      const dataId = querySnapshot.docs.map((doc) => doc.id)[0]
+      await deleteDoc(doc(followDB, dataId))
+      // 내 문서 팔로잉 감소
+      await updateDoc(myDoc, {
+        followingCount: increment(-1),
+      })
+      // 상대 유저 팔로워 감소
+      await updateDoc(followingDoc, {
+        followerCount: increment(-1),
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['user'],
+      })
+      queryClient.invalidateQueries({
+        queryKey: ['isFollowing'],
+      })
+    },
+  })
+
+  return (
+    <>
+      {isFollowing?.empty ? (
+        <Button onClick={() => mutateFollow.mutate()}>팔로우</Button>
+      ) : (
+        <Button variant={'destructive'} onClick={() => mutateUnfollow.mutate()}>
+          언팔로우
+        </Button>
+      )}
+    </>
+  )
+}
