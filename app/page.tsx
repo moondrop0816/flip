ba8bd5c0f23afd3a574bf77e14a09a861f75a4be
moondrop0816 <moondrop0816@gmail.com>
2024-05-09 17:16:30 +0900

    [DESIGN] 컨테이너 css 수정 #36

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ function Home() {
   const router = useRouter()
 
   return (
-    <div className='container bg-slate-300 w-screen h-screen flex flex-col justify-center items-center'>
+    <div className='bg-slate-300 w-screen h-screen flex flex-col justify-center items-center'>
       <h1 className='text-4xl font-bold text-center mb-10'>Flip</h1>
       <Button className='w-52 mb-2' onClick={() => router.push('/login')}>
         로그인
