a00cad29b95304bbe13bee35d73c40b2ec702c52
moondrop0816 <moondrop0816@gmail.com>
2024-05-24 22:39:16 +0900

    [FEAT] debounce 적용 후 자동완성 기능 구현 #40

--- app/(login)/search/page.tsx
+++ app/(login)/search/page.tsx
@@ -3,34 +3,43 @@
 import { UserCard } from '@/components/follow/userCard'
 import Icon from '@/components/icon'
 import { userDB } from '@/firebase/firebase'
-import { getDocs, or, query, where } from 'firebase/firestore'
+import { debounce } from '@/utils/debounce'
+import { getDocs, query, where } from 'firebase/firestore'
 import { useState } from 'react'
 
 const SearchPage = () => {
   const [keyword, setKeyword] = useState('')
   const [searchData, setSearchData] = useState<string[]>([])
-  // 1. 버튼 누르면 구현되는 방식으로 먼저 구현
-  // 2. 자동완성으로 바꾸기
-  const onSearch = async () => {
-    // 검색 함수
-    // 1. 클릭시 keyword로 검색하기
-    // 2. 파이어베이스로 요청 보내기
-    // 3. userDB에 해당 키워드랑 일치하는 닉네임 있는지? 유저 아이디 있는지? 체크해서 결과 리턴
-    // 유저 검색이니까 일단은 닉네임 + 유저 아이디로만 검색되게 하기
-    const q = query(
-      userDB,
-      or(
+
+  const debounceSearch = debounce(async (keyword) => {
+    if (keyword) {
+      console.log(keyword)
+
+      const userQ = query(
+        userDB,
         where('userId', '>=', keyword),
-        where('userId', '<=', keyword + '\uf8ff'),
+        where('userId', '<=', keyword + '\uf8ff')
+      )
+      const nicknameQ = query(
+        userDB,
         where('nickname', '>=', keyword),
         where('nickname', '<=', keyword + '\uf8ff')
       )
-    )
-    const querySnapshot = await getDocs(q)
-    const data = querySnapshot.docs.map((doc) => doc.id)
-    console.log(data)
+      const userSnap = await getDocs(userQ)
+      const nicknameSnap = await getDocs(nicknameQ)
+      const userData = userSnap.docs.map((doc) => doc.id)
+      const nickData = nicknameSnap.docs.map((doc) => doc.id)
+      setSearchData([...userData, ...nickData])
+    } else {
+      setSearchData([])
+    }
 
-    setSearchData(data)
+    console.log(searchData)
+  }, 500)
+
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value)
+    debounceSearch(e.target.value)
   }
 
   return (
@@ -42,12 +51,9 @@ const SearchPage = () => {
           placeholder='검색어를 입력해주세요'
           className='grow bg-transparent'
           value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
+          onChange={(e) => onSearch(e)}
         />
       </div>
-      <button type='button' onClick={onSearch}>
-        검색
-      </button>
       <div>
         {searchData.map((data) => {
           return <UserCard key={data} userUid={data} />
