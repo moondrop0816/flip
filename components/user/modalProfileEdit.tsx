8b752be4b8b72972c8d8ff47e3c829dcdfc1ac9e
moondrop0816 <moondrop0816@gmail.com>
2024-07-17 22:44:15 +0900

    [CHORE] 로그인한 유저의 userId, bio, nickname 변경 #41

--- components/user/modalProfileEdit.tsx
+++ components/user/modalProfileEdit.tsx
@@ -1,10 +1,3 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import Icon from '@/components/icon'
 import { Button } from '../ui/button'
@@ -22,8 +15,10 @@ import { useForm } from 'react-hook-form'
 import { useEffect, useState } from 'react'
 import { storage, userDB } from '@/firebase/firebase'
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
-import { DocumentData, getDocs, query, where } from 'firebase/firestore'
+import { doc, getDocs, query, updateDoc, where } from 'firebase/firestore'
 import { useLoginUserInfo } from '@/context/loginUserInfoProvider'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { usePathname, useRouter } from 'next/navigation'
 
 const formSchema = z.object({
   userId: z
@@ -52,47 +47,26 @@ const formSchema = z.object({
     .max(200),
 })
 
-export const ModalProfileEdit = () => {
-  const { loginUserInfo } = useLoginUserInfo()
+export const ModalProfileEdit = ({
+  closeModal,
+  uid,
+}: {
+  closeModal: () => void
+  uid: string | undefined
+}) => {
+  const { loginUserInfo, setLoginUserInfo } = useLoginUserInfo()
   const [imgPreview, setImgPreview] = useState(loginUserInfo?.profileImg)
   const [selectedFile, setSelectedFile] = useState<File | null>(null)
 
   const form = useForm<z.infer<typeof formSchema>>({
     mode: 'onBlur',
     resolver: zodResolver(formSchema),
-    // defaultValues: {
-    //   userId: loginUserInfo?.userId || '',
-    //   nickname: loginUserInfo?.nickname || '',
-    //   bio: loginUserInfo?.bio || '',
-    // },
+    defaultValues: {
+      userId: loginUserInfo?.userId || '',
+      nickname: loginUserInfo?.nickname || '',
+      bio: loginUserInfo?.bio || '',
+    },
   })
-
-  useEffect(() => {
-    if (loginUserInfo) {
-      setImgPreview(loginUserInfo.profileImg || '')
-      form.reset({
-        userId: loginUserInfo.userId || '',
-        nickname: loginUserInfo.nickname || '',
-        bio: loginUserInfo.bio || '',
-      })
-    }
-  }, [loginUserInfo, form])
-
-  const imageUpload = async (uid: string) => {
-    if (selectedFile) {
-      // 선택한 프로필 이미지가 있으면
-      const imageRef = ref(storage, `${uid}/${selectedFile.name}`)
-      await uploadBytes(imageRef, selectedFile)
-      const downloadURL = await getDownloadURL(imageRef)
-      return downloadURL
-    } else {
-      // 기본 프로필 이미지 주소 리턴
-      const defaultURL = await getDownloadURL(
-        ref(storage, '/defaultProfile.png')
-      )
-      return defaultURL
-    }
-  }
 
   const fileChange = (fileBlob: File) => {
     const reader = new FileReader()
@@ -124,19 +98,60 @@ export const ModalProfileEdit = () => {
     }
   }
 
-  const onEditProfile = async () => {
-    console.log('edit')
+  const onEditProfile = async (data: z.infer<typeof formSchema>) => {
+    console.log('이벤트 실행')
+    mutationEditProfile.mutate(data)
   }
 
+  const router = useRouter()
+  const mutationEditProfile = useMutation({
+    mutationFn: async (data: z.infer<typeof formSchema>) => {
+      const docRef = doc(userDB, uid)
+
+      const updateData: {
+        userId: string
+        nickname: string
+        bio: string
+        profileImg?: string
+      } = {
+        userId: data.userId,
+        nickname: data.nickname,
+        bio: data.bio,
+      }
+
+      if (selectedFile) {
+        const imageRef = ref(storage, `${uid}/profile`)
+        await uploadBytes(imageRef, selectedFile)
+        const downloadURL = await getDownloadURL(imageRef)
+        updateData['profileImg'] = downloadURL
+      }
+
+      setLoginUserInfo({
+        ...loginUserInfo,
+        ...updateData,
+      })
+
+      await updateDoc(docRef, updateData)
+    },
+    onMutate: () => {
+      closeModal()
+    },
+    onSuccess: () => {
+      const userId = form.getValues('userId')
+      router.push(`/${userId}`)
+      console.log('변경', loginUserInfo)
+    },
+  })
+
   return (
-    <Dialog>
-      {/* <DialogTrigger>
-        <Button variant='outline'>프로필 수정</Button>
-      </DialogTrigger> */}
-      <DialogContent className='sm:max-w-[425px]'>
-        <DialogHeader>
-          <DialogTitle>프로필 수정</DialogTitle>
-        </DialogHeader>
+    <div className='absolute top-0 left-0 right-0 bottom-0 z-10 bg-black/30'>
+      <div className='sm:max-w-[425px] bg-white p-5 rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <div className='flex justify-between items-center'>
+          <div className='text-lg font-bold'>프로필 수정</div>
+          <button onClick={closeModal}>
+            <Icon name='X' />
+          </button>
+        </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onEditProfile)}>
             <div className='mb-5 flex justify-center relative'>
@@ -227,14 +242,122 @@ export const ModalProfileEdit = () => {
               className='w-full text-lg h-12'
               disabled={
                 !form.formState.isValid ||
-                Object.keys(form.formState.dirtyFields).length === 0
+                (Object.keys(form.formState.dirtyFields).length === 0 &&
+                  selectedFile === null)
               }
             >
               저장하기
             </Button>
           </form>
         </Form>
-      </DialogContent>
-    </Dialog>
+      </div>
+    </div>
+    // <Dialog>
+    //   <DialogTrigger>
+    //     <Button variant='outline'>프로필 수정</Button>
+    //   </DialogTrigger>
+    //   <DialogContent className='sm:max-w-[425px]'>
+    //     <DialogHeader>
+    //       <DialogTitle>프로필 수정</DialogTitle>
+    //     </DialogHeader>
+    //     <Form {...form}>
+    //       <form onSubmit={form.handleSubmit(onEditProfile)}>
+    //         <div className='mb-5 flex justify-center relative'>
+    //           <div className='rounded-full overflow-hidden w-1/5'>
+    //             <img src={imgPreview} alt='프로필 이미지' />
+    //           </div>
+    //           <label className='bg-black flex self-end rounded-full p-3 cursor-pointer absolute left-[calc(50%+40px)]'>
+    //             <Icon name='ImagePlus' color='#fff' />
+    //             <input
+    //               type='file'
+    //               accept='image/*'
+    //               hidden={true}
+    //               onChange={(e) =>
+    //                 e.target.files && e.target.files.length > 0
+    //                   ? fileChange(e.target.files[0])
+    //                   : null
+    //               }
+    //             />
+    //           </label>
+    //         </div>
+    //         <div className='mb-2'>
+    //           <FormField
+    //             control={form.control}
+    //             name='userId'
+    //             render={({ field }) => (
+    //               <FormItem>
+    //                 <FormLabel>아이디</FormLabel>
+    //                 <div className='flex gap-2'>
+    //                   <FormControl>
+    //                     <Input type='text' placeholder='userId' {...field} />
+    //                   </FormControl>
+    //                   <Button
+    //                     type='button'
+    //                     onClick={userIdCheck}
+    //                     disabled={
+    //                       !form.getFieldState('userId').isDirty ||
+    //                       form.getFieldState('userId').invalid
+    //                     }
+    //                   >
+    //                     중복 확인
+    //                   </Button>
+    //                 </div>
+    //                 <FormMessage />
+    //               </FormItem>
+    //             )}
+    //           />
+    //         </div>
+    //         <div className='mb-2'>
+    //           <FormField
+    //             control={form.control}
+    //             name='nickname'
+    //             render={({ field }) => (
+    //               <FormItem>
+    //                 <FormLabel>닉네임</FormLabel>
+    //                 <FormControl>
+    //                   <Input
+    //                     type='text'
+    //                     placeholder='닉네임을 입력해 주세요.'
+    //                     {...field}
+    //                   />
+    //                 </FormControl>
+    //                 <FormMessage />
+    //               </FormItem>
+    //             )}
+    //           />
+    //         </div>
+    //         <div className='mb-8'>
+    //           <FormField
+    //             control={form.control}
+    //             name='bio'
+    //             render={({ field }) => (
+    //               <FormItem>
+    //                 <FormLabel>자기소개</FormLabel>
+    //                 <FormControl>
+    //                   <Input
+    //                     type='text'
+    //                     placeholder='자기소개를 입력해 주세요.'
+    //                     {...field}
+    //                   />
+    //                 </FormControl>
+    //                 <FormMessage />
+    //               </FormItem>
+    //             )}
+    //           />
+    //         </div>
+    //         <Button
+    //           type='submit'
+    //           className='w-full text-lg h-12'
+    //           disabled={
+    //             !form.formState.isValid ||
+    //             Object.keys(form.formState.dirtyFields).length === 0
+    //           }
+    //         >
+    //           저장하기
+    //         </Button>
+    //       </form>
+    //     </Form>
+    //   </DialogContent>
+    // </Dialog>
   )
 }
